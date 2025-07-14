2334ddeb27b0a66aa26b47a21eedceafbda8e4a7
Leyukezer Cruz <51835032+Leyukezer-Sys@users.noreply.github.com>
2025-07-14 18:12:00 -0400

    update

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Habilita CORS
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
--- src/modules/usuario/dto/update-usuario.dto.ts
+++ src/modules/usuario/dto/update-usuario.dto.ts
@@ -1,4 +1,12 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateUsuarioDto } from './create-usuario.dto';
+import { IsOptional } from 'class-validator';
 
-export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto){}
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {
+
+    @IsOptional()
+    contato?: string;
+
+    @IsOptional()
+    avaliacao?: number;
+}
